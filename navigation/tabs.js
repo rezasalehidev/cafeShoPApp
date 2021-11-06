e6ed8f4bbeb93cc34431f0a04064a8395b7f5599
rezasalehi <rezasalehidev@gmail.com>
2021-11-06 19:50:11 +0330

    add customBottomTabNavigator In Home Screen

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,31 +1,31 @@
 import React from "react";
-import {
-    Image,
-    Platform,
-} from "react-native";
-import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs"
+import { Image, Platform, View, Text } from "react-native";
+import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs";
 
-import { Home, Rewards } from "../screens"
-import { COLORS, SIZES, icons } from "../constants"
+import { Home, Rewards } from "../screens";
+import { COLORS, SIZES, icons } from "../constants";
+import { TouchableOpacity, TouchableWithoutFeedback } from "react-native-gesture-handler";
+import Svg, { Path } from "react-native-svg";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-
     return (
         <Tab.Navigator
             tabBarOptions={{
                 showLabel: false,
                 style: {
-                    position: 'absolute',
+                    position: "absolute",
                     bottom: 0,
                     left: 0,
                     right: 0,
                     elevation: 0,
-                    backgroundColor: COLORS.gray3,
+                    backgroundColor: "transparent",
                     borderTopColor: "transparent",
-                    height: (Platform.OS == 'android') ? 60 : 80
-                }
+                    height: Platform.OS == "android" ? 60 : 80,
+                    borderTopLeftRadius: SIZES.radius * 2,
+                    borderTopRightRadius: SIZES.radius * 2,
+                },
             }}
         >
             <Tab.Screen
@@ -33,15 +33,17 @@ const Tabs = () => {
                 component={Home}
                 options={{
                     tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.home}
-                            resizeMode="contain"
-                            style={{
-                                width: 35,
-                                height: 35,
-                                tintColor: focused ? COLORS.primary : COLORS.black
-                            }}
-                        />
+                        <View style={{ backgroundColor: COLORS.gray3, height: 60, width: "100%", alignItems: "center", justifyContent: "center", padding: 7, borderTopLeftRadius: 20 }}>
+                            <Image
+                                source={icons.home}
+                                resizeMode="contain"
+                                style={{
+                                    width: 35,
+                                    height: 35,
+                                    tintColor: focused ? COLORS.white : COLORS.black,
+                                }}
+                            />
+                        </View>
                     ),
                 }}
             />
@@ -50,15 +52,17 @@ const Tabs = () => {
                 component={Rewards}
                 options={{
                     tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.bubbleTea}
-                            resizeMode="contain"
-                            style={{
-                                width: 35,
-                                height: 35,
-                                tintColor: focused ? COLORS.primary : COLORS.black
-                            }}
-                        />
+                        <View style={{ backgroundColor: COLORS.gray3, height: "100%", width: "100%", alignItems: "center", justifyContent: "center", marginRight: 16 }}>
+                            <Image
+                                source={icons.bubbleTea}
+                                resizeMode="contain"
+                                style={{
+                                    width: 35,
+                                    height: 35,
+                                    tintColor: focused ? COLORS.white : COLORS.black,
+                                }}
+                            />
+                        </View>
                     ),
                 }}
             />
@@ -68,15 +72,28 @@ const Tabs = () => {
                 component={Home}
                 options={{
                     tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.add}
-                            resizeMode="contain"
-                            style={{
-                                width: 35,
-                                height: 35,
-                                tintColor: COLORS.white
-                            }}
-                        />
+                        <View>
+                            <Image
+                                source={icons.add}
+                                resizeMode="contain"
+                                style={{
+                                    width: 35,
+                                    height: 35,
+                                    tintColor: COLORS.gray,
+                                    position: "absolute",
+                                    top: -20,
+                                    left: 25,
+                                    tintColor: focused ? COLORS.yellow : COLORS.black,
+                                }}
+                            />
+                            <Svg xmlns="http://www.w3.org/2000/svg" width={90} height={60} viewBox="0 0 90 61">
+                                <Path
+                                    d="M0 0a38.742 38.742 0 0113 7c5.313 4.4 6.7 8.593 12 13 5.993 4.98 12.987 8 20 8s14.007-3.02 20-8c5.3-4.408 6.687-8.6 12-13a38.742 38.742 0 0113-7v61H0V0z"
+                                    fill="#4d4d4d"
+                                    fillRule="evenodd"
+                                />
+                            </Svg>
+                        </View>
                     ),
                 }}
             />
@@ -85,15 +102,17 @@ const Tabs = () => {
                 component={Home}
                 options={{
                     tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.heart}
-                            resizeMode="contain"
-                            style={{
-                                width: 35,
-                                height: 35,
-                                tintColor: focused ? COLORS.primary : COLORS.black
-                            }}
-                        />
+                        <View style={{ backgroundColor: COLORS.gray3, height: "100%", width: "100%", alignItems: "center", justifyContent: "center", marginLeft: 16 }}>
+                            <Image
+                                source={icons.heart}
+                                resizeMode="contain"
+                                style={{
+                                    width: 35,
+                                    height: 35,
+                                    tintColor: focused ? COLORS.white : COLORS.black,
+                                }}
+                            />
+                        </View>
                     ),
                 }}
             />
@@ -102,20 +121,22 @@ const Tabs = () => {
                 component={Home}
                 options={{
                     tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.profile}
-                            resizeMode="contain"
-                            style={{
-                                width: 35,
-                                height: 35,
-                                tintColor: focused ? COLORS.primary : COLORS.black
-                            }}
-                        />
+                        <View style={{ backgroundColor: COLORS.gray3, height: "100%", width: "100%", alignItems: "center", justifyContent: "center", borderTopRightRadius: 20 }}>
+                            <Image
+                                source={icons.profile}
+                                resizeMode="contain"
+                                style={{
+                                    width: 35,
+                                    height: 35,
+                                    tintColor: focused ? COLORS.white : COLORS.black,
+                                }}
+                            />
+                        </View>
                     ),
                 }}
             />
         </Tab.Navigator>
-    )
-}
+    );
+};
 
 export default Tabs;
